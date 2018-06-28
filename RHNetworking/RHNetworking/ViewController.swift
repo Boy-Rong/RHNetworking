870cae6c632affa4727fa71887b321f37e9a4f79
荣恒 <rongheng.rh@gmail.com>
2018-06-28 14:31:08 +0800

    1.初始化项目

---
+++ RHNetworking/RHNetworking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RHNetworking
+//
+//  Created by 荣恒 on 2018/6/28.
+//  Copyright © 2018 荣恒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
